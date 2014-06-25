b72193bd604823a6651195c710ef6e6e48f9a2ab
prestrepoh <prestrepoh@gmail.com>
2014-06-25 15:33:16 -0500

    added a programm to crack by brute force Unix encrypted passwords and SHA-15 encrypted passwords

---
+++ chapter-1/passwordCracker.py
@@ -0,0 +1,29 @@
+'''
+This programm encripts the passwords from a given list, and then compares them
+to perform a brute-force attack
+'''
+import crypt
+
+def testPass(cryptPass):
+  salt = cryptPass[0:2]
+  dictFile = open('dictionary.txt')
+  for word in dictFile.readlines():
+    word = word.strip('\n')
+    cryptWord = crypt.crypt(word,salt)
+    if (cryptWord == cryptPass):
+      print "Found password" + word + '\n'
+      return
+  print "password not found \n"
+  return
+
+def main():
+  passFile = open('passwords.txt')
+  for line in passFile.readlines():
+    if ":" in line:
+      user = line.split(':')[0]
+      cryptPass = line.split(':')[1].strip(' ')
+      print "cracking password for: " + user
+      testPass(cryptPass)
+
+if __name__ == "__main__":
+  main()
