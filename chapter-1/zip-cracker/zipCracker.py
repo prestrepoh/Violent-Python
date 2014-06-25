149372e69a25d32588a10007de539fe59436d098
prestrepoh <prestrepoh@gmail.com>
2014-06-25 16:55:51 -0500

    added a zip password cracker

--- chapter-1/zip-cracker/zipCracker.py
+++ chapter-1/zip-cracker/zipCracker.py
@@ -1,3 +1,34 @@
 import zipfile
-zFile= zipfile.ZipFile("evil.zip")
-#zFile.extractall(pwd="secret")
+import optparse
+import sys
+from threading import Thread
+
+def extractFile(zFile,password):
+  try:
+    zFile.extractall(pwd=password)
+    print '[+] found password: ' + password
+  except:
+    pass
+
+def main():
+  parser = optparse.OptionParser(sys.argv[0] + \
+  " -f <zipfile> -d <dictionary>")
+  parser.add_option('-f',dest='zname',type='string',help='specify zip file')
+  parser.add_option('-d',dest='dname',type='string',\
+  help='specify dictionary file')
+  (options, args) = parser.parse_args()
+  if(options.zname == None) | (options.dname == None):
+    print "usage: " + parser.usage
+    exit(0)
+  else:
+    zname = options.zname
+    dname = options.dname
+    zFile=zipfile.ZipFile(zname)
+    passFile = open(dname)
+    for line in passFile.readlines():
+      password = line.strip('\n')
+      t = Thread(target=extractFile, args=(zFile,password))
+      t.start()
+
+if __name__ == '__main__':
+  main()
